c7f29059d9a1f370a74cd7b3661db9167315293c
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-18 16:52:15 +0500

    add config.

---
+++ app/core/config.py
@@ -0,0 +1,27 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn, computed_field
+from pydantic_core import MultiHostUrl
+
+
+class Settings(BaseSettings):
+    DB_USER: str
+    DB_PASSWORD: str
+    DB_HOST: str
+    DB_NAME: str
+
+    @computed_field
+    @property
+    def asyncpg_url(self) -> PostgresDsn:
+        return MultiHostUrl.build(
+            scheme="postgresql+asyncpg",
+            username=self.DB_USER,
+            password=self.DB_PASSWORD,
+            host=self.DB_HOST,
+            path=self.DB_NAME,
+        )
+        
+    model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, case_sensitive=True)
+    
+    
+settings = Settings()
+    
