302f0859429a6d0f489ed34195e3a52c455990d5
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-09-18 20:37:48 +0500

    add testing get_wallet route.

--- app/api/routers/wallets.py
+++ app/api/routers/wallets.py
@@ -1,5 +1,9 @@
+from fastapi import Depends
 from fastapi.routing import APIRouter
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.core.database import get_db
+from app import models
 
 router = APIRouter(
     prefix="/wallets",
@@ -8,5 +12,8 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_wallet():
-    return {"success": True}
+async def get_wallet(session: AsyncSession = Depends(get_db)):
+    stmt = select(models.Wallet)
+    result = await session.execute(stmt)
+    wallets = result.scalars().all()
+    return {"wallets": wallets}
